4410d6e6ce7aad72c6fb4dcd1f6b96ea3c8ff345
jiuraque <jiulianna.raquel@gmail.com>
2025-03-16 00:17:00 -0300

    viendo si funciona la funcion de updatetasklist

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,59 @@ const taskID = document.getElementById("taskIds");
 const taskName = document.getElementById("taskNames");
 const taskAction = document.getElementById("taskActions");
 
+taskBtn.addEventListener("click", () => {
+    const newTask = taskInput.value.trim();  
+
+    if (newTask === "") {
+        alert("Por favor, ingresa una tarea.");
+        return;
+    }
+
+    addID++;
+    task.push({ id: addID, name: newTask, completed: false });
+    taskInput.value = "";
+    updateTaskList();
+});
+
+function updateTaskList() {
+    taskID.innerHTML = "";
+    taskName.innerHTML = "";
+    taskAction.innerHTML = "";
+
+    let totalTasks = task.length;
+    let completedTasks = task.filter(task => task.completed).length;
+
+    task.forEach (task => {
+        const taskIdElement = document.createElement("li");
+        taskIdElement.textContent = task.id;
+
+        const taskNameElement = document.createElement("li");
+        taskNameElement.textContent = task.name;
+
+        const taskActionElement = document.createElement("li");
+
+        const taskCheckbox = document.createElement("input");
+        taskCheckbox.type = "checkbox";
+        taskCheckbox.checked = task.completed; 
+        taskCheckbox.addEventListener("change", () => {
+            task.completed = taskCheckbox.checked;  
+            updateTaskList(); 
+    });
+
+    const deleteButton = document.createElement("button");
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("fas", "fa-xmark");
+    deleteButton.appendChild(deleteIcon);
+
+    deleteButton.addEventListener("click", () => {
+        deleteTask(task.id);
+    });
+
+    taskActionElement.appendChild(taskCheckbox);
+    taskActionElement.appendChild(deleteButton);
+});
+
+    updateCounters(totalTasks, completedTasks);
+}
+
+console.log(updateTaskList)
