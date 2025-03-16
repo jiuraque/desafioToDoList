f919de3c55b1e5dbd5e5484c59bdf5cf1726c982
jiuraque <jiulianna.raquel@gmail.com>
2025-03-16 01:05:12 -0300

    se agregan las 3 tareas iniciales en el arreglo  que se mencionan en el desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-let task = [];
-let addID = 0;
+let task = [
+    {id: 1, name: "Peinar a Wanda", check: false},
+    {id: 2, name: "Hacer el Almuerzo", check: false},
+    {id: 3, name: "Estudiar JavaScript", check: false},
+];
+
+let addID = task.length;
 
 const taskInput = document.getElementById("taskInput");
 const taskBtn = document.getElementById("addTaskBtn");
@@ -17,18 +22,12 @@ taskBtn.addEventListener("click", () => {
         return;
     }
 
-    
     addID++;
-
-    
+ 
     task.push({ id: addID, name: newTask, completed: false });
-
-    console.log(task);
-
-    
+ 
     taskInput.value = "";
 
-    
     updateTaskList();
 });
 
@@ -37,35 +36,26 @@ function updateTaskList() {
     taskID.innerHTML = "";
     taskName.innerHTML = "";
     taskAction.innerHTML = "";
-
     
     let totalTasks = task.length;
     let completedTasks = task.filter(task => task.completed).length;
 
-    
     task.forEach(task => {
         
         const taskIdElement = document.createElement("li");
         taskIdElement.textContent = task.id;
-
        
         const taskNameElement = document.createElement("li");
         taskNameElement.textContent = task.name;
-
-        
+      
         const taskActionElement = document.createElement("li");
-        
-       
+              
         const taskCheckbox = document.createElement("input");
 
         taskCheckbox.type = "checkbox";
         taskCheckbox.checked = task.completed; 
         taskCheckbox.addEventListener("change", () => {
             task.completed = taskCheckbox.checked;  
-
-            console.log(task.completed);
-            console.log(task);
-
 
             updateTaskList();  
         });
@@ -77,20 +67,17 @@ function updateTaskList() {
 
         deleteButton.addEventListener("click", () => {
             deleteTask(task.id);
-            task.completed ? taskCheckbox.checked : alert("Esta tarea no está completada, ¿estás seguro que quieres eliminarla?");
+            
         });
-
-        
+       
         taskActionElement.appendChild(taskCheckbox);
         taskActionElement.appendChild(deleteButton);
-
-        
+       
         taskID.appendChild(taskIdElement);
         taskName.appendChild(taskNameElement);
         taskAction.appendChild(taskActionElement);
     });
-
-    
+ 
     updateCounters(totalTasks, completedTasks);
 }
 
@@ -100,11 +87,10 @@ function updateCounters(total, completed) {
     taskComplete.textContent = completed;
 }
 
-
 function deleteTask(taskId) {
     
     task = task.filter(t => t.id !== taskId);
-
-    
+    task.completed ? taskCheckbox.checked : alert("Esta tarea no está completada, ¿estás seguro que quieres eliminarla?");
+   
     updateTaskList();
 }
