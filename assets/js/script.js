65a6c90a338e90c27160811b554da0ce17ef8cab
jiuraque <jiulianna.raquel@gmail.com>
2025-03-16 00:53:19 -0300

    se agregan las funciones para los contadores y  para eliminar tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,51 +17,94 @@ taskBtn.addEventListener("click", () => {
         return;
     }
 
+    
     addID++;
+
+    
     task.push({ id: addID, name: newTask, completed: false });
+
+    console.log(task);
+
+    
     taskInput.value = "";
+
+    
     updateTaskList();
 });
 
 function updateTaskList() {
+    
     taskID.innerHTML = "";
     taskName.innerHTML = "";
     taskAction.innerHTML = "";
 
+    
     let totalTasks = task.length;
     let completedTasks = task.filter(task => task.completed).length;
 
-    task.forEach (task => {
+    
+    task.forEach(task => {
+        
         const taskIdElement = document.createElement("li");
         taskIdElement.textContent = task.id;
 
+       
         const taskNameElement = document.createElement("li");
         taskNameElement.textContent = task.name;
 
+        
         const taskActionElement = document.createElement("li");
-
+        
+       
         const taskCheckbox = document.createElement("input");
+
         taskCheckbox.type = "checkbox";
         taskCheckbox.checked = task.completed; 
         taskCheckbox.addEventListener("change", () => {
             task.completed = taskCheckbox.checked;  
-            updateTaskList(); 
+
+            console.log(task.completed);
+            console.log(task);
+
+
+            updateTaskList();  
+        });
+        
+        const deleteButton = document.createElement("button");
+        const deleteIcon = document.createElement("i");
+        deleteIcon.classList.add("fas", "fa-xmark");  
+        deleteButton.appendChild(deleteIcon);
+
+        deleteButton.addEventListener("click", () => {
+            deleteTask(task.id);
+            task.completed ? taskCheckbox.checked : alert("Esta tarea no está completada, ¿estás seguro que quieres eliminarla?");
+        });
+
+        
+        taskActionElement.appendChild(taskCheckbox);
+        taskActionElement.appendChild(deleteButton);
+
+        
+        taskID.appendChild(taskIdElement);
+        taskName.appendChild(taskNameElement);
+        taskAction.appendChild(taskActionElement);
     });
 
-    const deleteButton = document.createElement("button");
-    const deleteIcon = document.createElement("i");
-    deleteIcon.classList.add("fas", "fa-xmark");
-    deleteButton.appendChild(deleteIcon);
-
-    deleteButton.addEventListener("click", () => {
-        deleteTask(task.id);
-    });
-
-    taskActionElement.appendChild(taskCheckbox);
-    taskActionElement.appendChild(deleteButton);
-});
-
+    
     updateCounters(totalTasks, completedTasks);
 }
 
-console.log(updateTaskList)
+function updateCounters(total, completed) {
+   
+    taskTotal.textContent = total;
+    taskComplete.textContent = completed;
+}
+
+
+function deleteTask(taskId) {
+    
+    task = task.filter(t => t.id !== taskId);
+
+    
+    updateTaskList();
+}
